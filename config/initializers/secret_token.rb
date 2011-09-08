b2d2430ec0f64b3db2631b0b2536f937847efaac
Sean Carroll <sfcarroll@gmail.com>
2011-09-08 15:44:06 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Factura::Application.config.secret_token = 'fc84c757ec3e8fc11ae69543601eb65c77065b2169c136da75119faaeaa8a3573edf5c8dfbfdd632534ee38bcc15130c46145b1909a97262c5c816522bd6e019'
